cc22c0fab02e06dc212721d02773519fb3e028d5
KWON-WOO <ckw075@naver.com>
2025-12-17 16:06:32 +0900

    ✨ feat: Level.1-2. 코드 추가 퀴즈 - JWT의 이해 해결
    - User의 정보에 nickname이 필요해졌어요.
        - User 테이블에 nickname 컬럼을 추가해주세요.
        - nickname은 중복 가능합니다.
    - 프론트엔드 개발자가 JWT에서 유저의 닉네임을 꺼내 화면에 보여주길 원하고 있어요.
    
    요구사항 반영

--- src/main/java/org/example/expert/domain/todo/service/TodoService.java
+++ src/main/java/org/example/expert/domain/todo/service/TodoService.java
@@ -43,7 +43,7 @@ public class TodoService {
                 savedTodo.getTitle(),
                 savedTodo.getContents(),
                 weather,
-                new UserResponse(user.getId(), user.getEmail())
+                new UserResponse(user)
         );
     }
 
@@ -57,7 +57,7 @@ public class TodoService {
                 todo.getTitle(),
                 todo.getContents(),
                 todo.getWeather(),
-                new UserResponse(todo.getUser().getId(), todo.getUser().getEmail()),
+                new UserResponse(todo.getUser()),
                 todo.getCreatedAt(),
                 todo.getModifiedAt()
         ));
@@ -74,7 +74,7 @@ public class TodoService {
                 todo.getTitle(),
                 todo.getContents(),
                 todo.getWeather(),
-                new UserResponse(user.getId(), user.getEmail()),
+                new UserResponse(user),
                 todo.getCreatedAt(),
                 todo.getModifiedAt()
         );
--- src/main/java/org/example/expert/domain/user/dto/response/UserResponse.java
+++ src/main/java/org/example/expert/domain/user/dto/response/UserResponse.java
@@ -1,15 +1,18 @@
 package org.example.expert.domain.user.dto.response;
 
 import lombok.Getter;
+import org.example.expert.domain.user.entity.User;
 
 @Getter
 public class UserResponse {
 
     private final Long id;
     private final String email;
+    private final String nickname;
 
-    public UserResponse(Long id, String email) {
-        this.id = id;
-        this.email = email;
+    public UserResponse(User user) {
+        this.id = user.getId();
+        this.email = user.getEmail();
+        this.nickname = user.getNickname();
     }
 }
